00da1636074b566b45f059f02c0c56dcd11b820b
moksha <dullstart.yang@gmail.com>
2022-11-14 11:19:25 +0800

    簡單表格

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">   
+    <link rel="stylesheet" href="style.css" type="text/css">   
     <title>學生管理系統</title>
 
 </head>
@@ -14,15 +14,38 @@
 $dsn="mysql:host=localhost;charset=utf8;dbname=school";
 $pdo= new PDO($dsn,'moksha','ji308120');//連線物件 PDO();
 
-$sql="SELECT * FROM `students` ";
-$result=$pdo->query($sql);//連線到資料表data了
-$rows=$result->fetch(PDO::FETCH_NAMED);//對資料表fetch(拿取資料);
-$rows=$result->fetch(PDO::FETCH_NAMED);
-// $rows=$pdo->query($sql)->fetchAll(PDO::FETCH_NAMED);
+$sql="SELECT * FROM `students` LIMIT 20 ";
+//$result=$pdo->query($sql);//連線到資料表data了
+//$rows=$result->fetch(PDO::FETCH_NAMED);//對資料表fetch(拿取資料);
+//$rows=$result->fetch(PDO::FETCH_NAMED);
+ $rows=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 
-echo "<pre>";
-print_r($rows);
-echo "</pre>";
+// echo "<pre>";
+// print_r($rows);
+// echo "</pre>";
 ?>
+
+<table border="1" id="list-students">
+    <tr>
+        <td>學號</td>
+        <td>姓名</td>
+        <td>生日</td>
+        <td>畢業國中代碼</td>
+        <td>年齡</td>
+    </tr>
+<?php
+foreach($rows as $row){
+    $age=round((strtotime('now')-strtotime($row['birthday']))/(60*60*24*365),1);
+
+    echo "<tr>";
+    echo "<td>{$row['school_num']}</td>";
+    echo "<td>{$row['name']}</td>";
+    echo "<td>{$row['birthday']}</td>";
+    echo "<td>{$row['graduate_at']}</td>";
+    echo "<td>{$age}</td>";
+    echo "</tr>";
+}
+?>
+</table>
 </body>
 </html>

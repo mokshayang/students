b10beab2ae1c5e9178021a80f6ac53617e6b8443
moksha <dullstart.yang@gmail.com>
2022-11-14 09:01:45 +0800

    連線

--- index.php
+++ index.php
@@ -11,12 +11,16 @@
 <body>
     <h1>學生管理系統</h1>
     <?php
-$dsn="mysql:host=location;charset=utf8;dbname=school";
+$dsn="mysql:host=localhost;charset=utf8;dbname=school";
 $pdo= new PDO($dsn,'moksha','ji308120');
 
-$sql="SELECT * FROM `students` LINIT 5";
+$sql="SELECT * FROM `students` LIMIT 5";
 
+$rows=$pdo->query($sql)->fetchALL();
 
+echo "<pre>";
+print_r($rows);
+echo "</pre>";
 ?>
 </body>
 </html>
